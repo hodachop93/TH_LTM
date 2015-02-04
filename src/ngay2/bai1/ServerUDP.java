2e10dee534b51c79d43fdcb364cd1c47271901dd
hodachop93 <hodachop93@gmail.com>
2015-02-04 22:07:41 +0700

    4/2
    4/2

--- src/ngay2/bai1/ServerUDP.java
+++ src/ngay2/bai1/ServerUDP.java
@@ -1,5 +1,20 @@
 package ngay2.bai1;
 
+import java.net.*;
 public class ServerUDP {
-
+	public final static int port = 3000;
+	public static void main(String[] args) throws Exception{
+		//Tao thung thu de nhan thu
+		DatagramSocket serverSocket = new DatagramSocket(port);
+		
+		//Tao 2 stream de giao tiep
+		byte[] receiveData = new byte[1024];
+		byte[] sendData = new byte[1024];
+		
+		//Tien hanh qua trinh nhan du lieu
+		while (true){
+			//Tao mot goi buu pham de nhan
+			DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+		}
+	}
 }
